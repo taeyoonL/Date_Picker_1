9aee56f8100151d3accb323cd1a95f375bc8feb3
taeyoonL <tylee3715@gmail.com>
2023-07-05 11:30:26 +0900

    Initial Commit

---
+++ my_practice_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my_practice_1
+//
+//  Created by 이태윤 on 2023/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
